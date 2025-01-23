8b6170c4975410ea78ae1c1e310ac5a35bdf91a2
Ricardo Ochoa <ricardo8arr@gmail.com>
2025-01-23 00:53:41 -0600

    created register endpoint

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,21 @@
 import {Request, Response} from 'express'
+import User from '../models/User'
+import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-    console.log("function createAccount call")
+    
+    const {email, password} = req.body
+
+    const userExists = await User.findOne({email})
+    if(userExists) {
+        const error = new Error('The user already exists')
+        res.status(409).json({error: error.message})
+        return;
+    }
+
+    const user = new User(req.body)
+    user.password = await hashPassword(password)
+
+    await user.save()
+    res.status(201).send('User created!')
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { body } from "express-validator";
 import { createAccount } from "./handlers";
+import { handleInputErrors } from "./middleware/validation";
 
 const router = Router()
 
@@ -8,6 +9,18 @@ router.get('/', function(req, res) {
     res.send('Hello world, the API is alive!')
 })
 
-router.post('/auth/register', createAccount)
+router.post('/auth/register',
+    body('name')
+        .notEmpty()
+        .withMessage(`The name field can't be empty`),   
+    body('email')
+        .isEmail()
+        .withMessage('The email is not valid'),
+    body('password')
+        .isLength({min: 8, max: 20})
+        .withMessage("Password length must be between 8 and 20 characters"),
+    handleInputErrors,
+    createAccount
+)
 
 export default router
